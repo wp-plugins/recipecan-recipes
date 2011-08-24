9fb4d9a3c0d2a928ae601cd74b50a0b87378017d
ryanto <ryanto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-24 04:35:39 +0000

    commit files, see git for history
    git-svn-id: https://plugins.svn.wordpress.org/recipecan-recipes/trunk@427945 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- recipecan-recipes.php
+++ recipecan-recipes.php
@@ -1,18 +1,18 @@
 <?php
 /*
   Plugin Name: RecipeCan Recipes
-  Plugin URI: http://www.recipecan.com
+  Plugin URI: http://www.recipecan.com/howto/recipe-wordpress-plugin
   Description: A WordPress plugin that organizes recipes on your blog.
-  Version: 0.1.1
+  Version: 0.1.11
   Author: Ryan (ryanto)
   Author URI: http://www.recipecan.com
  */
 
-define('RECIPECAN_VERSION', '0.1.1');
+define('RECIPECAN_VERSION', '0.1.11');
 
 
 $recipecan_options = array(
-    'plugin_url' => plugin_dir_url(__FILE__),
+    'plugin_url' => plugins_url() . "/recipecan-recipes/",
     'plugin_version' => RECIPECAN_VERSION,
     'path' => dirname(__FILE__),
     'request' => $_REQUEST,
